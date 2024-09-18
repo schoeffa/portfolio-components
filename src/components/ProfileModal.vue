beee8de56c4e346ab6e1a729fa7f1d8e72109fd8
aaron <schoeffa@gmail.com>
2024-09-18 14:49:36 -0400

    modal component

---
+++ src/components/ProfileModal.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { isDarkMode = false } = defineProps({ isDarkMode: Boolean, isVisible: Boolean });
+
+const classes = ['profile-modal-overlay', { 'profile-modal-overlay--dark-mode': isDarkMode }];
+</script>
+
+<template>
+  <transition name="modal-fade">
+    <div v-if="isVisible" :class="classes" @click="$emit('close')">
+      <div class="profile-modal" @click.stop>
+        <button class="profile-modal-close" @click="$emit('close')">✖</button>
+        <div class="profile-modal-content">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
